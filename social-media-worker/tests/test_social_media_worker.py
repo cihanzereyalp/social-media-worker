f08f30ef8373b14e311788b5d0b2c6a15d9f715a
cihanzereyalp <cihan.zereyalp@logsign.net>
2019-06-07 01:04:51 +0300

    test: add start scenario

--- social-media-worker/tests/test_social_media_worker.py
+++ social-media-worker/tests/test_social_media_worker.py
@@ -1,13 +1,21 @@
 import unittest
-from unittest import TestCase, mock
-from unittest.mock import MagicMock
-from unittest.mock import patch
+from unittest import mock
+
+from .. import login_with_creds
+from .. import main
 
 
-class SMWorker(unittest.TestCase):
-    def test_sm_instagram(self):
-        pass
+class TestSMWorker(unittest.TestCase):
+    @mock.patch.object(login_with_creds.LoginWithCreds, '__init__')
+    @mock.patch('social-media-worker.main.get_inputs')
+    def test_main_start_instagram(self, mock_get_inputs, mock_class):
+        mock_get_inputs.return_value = 'instagram', 'instagram', 'instagram'
+        mock_class.return_value = None
+
+        main.start()
+        mock_get_inputs.assert_called()
+        mock_class.assert_called_once_with('instagram', 'instagram', 'instagram')
 
 
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
